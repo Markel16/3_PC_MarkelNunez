6f00b1fc7e7c195b0b03fd0135b7084edc49bd8e
Markel16 <markel_nunez@euneiz.com>
2025-12-09 18:14:53 +0100

    llamada a las nuevas clases de camara y terreno

--- include/App.h
+++ include/App.h
@@ -13,40 +13,47 @@
 // Cubo 3D que rota
 #include "CuboRotado.h"
 
+//Entrega PDF
+#include "Camara.h"
+#include "Terreno.h"
+
 class App
 {
 public:
     App();
     ~App();
 
-    void run();    // arranca la app (llama a mainLoop)
+    void run();   
 
 private:
-    // --- Estado general de la aplicación ---
+    //Estado general de la aplicación
     GLFWwindow* window = nullptr;
     GLuint shaderProgram = 0;   // shader compartido por las figuras
 
-    // --- Figuras en pantalla ---
+    //Figuras en pantalla
     CuadradoMix    cuadradoMix;     // cuadrado mezcla de 2 texturas + color
     CuadradoRotado cuadradoRotado;  // cuadrado con textura rotada
     Circulo        circulo;         // círculo sólido
     TrianguloRGB   trianguloRGB;    // triángulo con 3 colores
     CuboRotado    cubo;            // cubo que rota
 
-    // --- Ciclo de vida ---
+	//Llamada de la Entrega PDF
+	Camara camara;
+	Terreno terreno;
+
+    //Para el deltaTime
+	double lastTime = 0.0;
+
+    //Ciclo de vida
     void init();        // inicializa GLFW, GLAD, shaders y figuras
     void mainLoop();    // bucle principal (draw de todas las figuras)
     void cleanup();     // libera recursos
 
-    // --- Callbacks ---
+    //Callbacks
     // Debe ser static porque se pasa directamente a glfwSetKeyCallback
-    static void KeyCallback(GLFWwindow* window,
-        int key,
-        int scancode,
-        int action,
-        int mods);
+    static void KeyCallback(GLFWwindow* window,int key,int scancode,int action, int mods);
 
-    // --- Shaders / utilidades ---
-    void initShaders();                            // compila y linka shaderProgram
-    std::string loadShaderSource(const std::string& path); // lee el código de los .vs/.fs
+    //Shaders / utilidades
+    void initShaders(); // compila y linka shaderProgram
+    std::string loadShaderSource(const std::string& path); // lee el codigo de los .vs/.fs
 };
